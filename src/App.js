86fb09321c368aba6d8b3ecb3fba194f793a177c
Lena Melya <lenomeltashevich@gmail.com>
2024-11-27 00:07:21 +0300

    add html scss and a little react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import "./App.css";
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizaBlock from "./components/PIzzaBlock";
 
-import './scss/app.scss'
 function App() {
   return (
     <div className="App">
-     <h1>Hello World!</h1>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+             <PizaBlock title="Мексиканская" price="500" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
